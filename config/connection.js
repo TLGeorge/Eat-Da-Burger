f88fe9f24811813475e6fbd4a463843c2f5ec2ba
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-05-29 18:48:12 -0400

    connection.js

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+
+// Sets up MySQL connection
+const mysql = require("mysql");
+
+// Sets up secure password
+const dotenv = require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+// Connect to MySql
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
